cf8f48703681b3b3938db21236a7593192f25c8b
root <root@web.2pz.es>
2026-07-03 17:19:00 +0000

    [FOUNDATION] Initial FD Booking Beauty vertical

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "FD Booking Beauty",
+    "summary": "Beauty and aesthetics vertical for FD Booking Engine",
+    "version": "17.0.0.1.0",
+    "category": "Services/Booking",
+    "author": "Factor Digital",
+    "website": "https://factordigital.es",
+    "license": "LGPL-3",
+    "depends": [
+        "fd_booking",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/beauty_demo_data.xml",
+        "views/beauty_menu_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
